785bd267118ca4ec50051d442690489b249fe27b
akshato7 <akshathg7@gmail.com>
2025-03-31 17:50:34 +0530

    package configuration changed from type:module causing problme with github auth provide

--- frontend/app/api/auth/[...nextauth]/route.js
+++ frontend/app/api/auth/[...nextauth]/route.js
@@ -1,9 +1,9 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import GitHubProvider from "next-auth/providers/github";
 
 const handler = NextAuth({
   providers: [
-    GithubProvider({
+    GitHubProvider({
       clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET,
       scope: "repo read:user",
